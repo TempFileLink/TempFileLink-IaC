513ccb126a0c6ee7e2bfec19646e004d98e006ef
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2024-12-06 00:38:08 +0700

    ops: add https listener for alb

--- balancer.tf
+++ balancer.tf
@@ -10,10 +10,24 @@ resource "aws_lb" "ecs_alb" {
   }, local.common_tags)
 }
 
-resource "aws_lb_listener" "ecs_alb_listener" {
+resource "aws_lb_listener" "ecs_alb_listener_http" {
   load_balancer_arn = aws_lb.ecs_alb.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+  }
+}
+
+resource "aws_lb_listener" "ecs_alb_listener_https" {
+  count             = var.acm_certificate_domain_name != null ? 1 : 0
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.cert[0].arn
 
   default_action {
     type             = "forward"
