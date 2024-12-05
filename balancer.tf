0a8f2987b7caf5179639392e10fcb7db342d4309
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2024-12-06 01:05:10 +0700

    ops: add dynamic default action for http listener in alb

--- balancer.tf
+++ balancer.tf
@@ -15,14 +15,30 @@ resource "aws_lb_listener" "ecs_alb_listener_http" {
   port              = 80
   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.ecs_tg.arn
+  dynamic "default_action" {
+    for_each = length(aws_acm_certificate.cert) == 1 ? [] : [1]
+    content {
+      type             = "forward"
+      target_group_arn = aws_lb_target_group.ecs_tg.arn
+    }
+  }
+
+  dynamic "default_action" {
+    for_each = length(aws_acm_certificate.cert) == 1 ? [1] : []
+    content {
+      type = "redirect"
+
+      redirect {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
   }
 }
 
 resource "aws_lb_listener" "ecs_alb_listener_https" {
-  count             = var.acm_certificate_domain_name != null ? 1 : 0
+  count             = length(aws_acm_certificate.cert)
   load_balancer_arn = aws_lb.ecs_alb.arn
   port              = "443"
   protocol          = "HTTPS"
