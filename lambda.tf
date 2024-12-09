e92cb83db83c822ffa2247bc7162974d6dfcc76c
SyahrulApr86 <94279620+SyahrulApr86@users.noreply.github.com>
2024-12-09 12:37:19 +0700

    Setup lambda (#1)
    * ops: add configuration to setup lambda
    
    * fix(main): update organization name from 'kowan123' to 'kowan'
    
    * style: format files with terraform fmt
    
    * fix: resolve missing function.zip error in aws_lambda_function resource
    
    * fix: resolve missing function.zip error in aws_lambda_function resource

---
+++ lambda.tf
@@ -0,0 +1,49 @@
+resource "aws_lambda_function" "s3_cleanup" {
+  filename         = "function.zip"
+  source_code_hash = filebase64sha256("function.zip")
+  function_name    = "s3-cleanup-function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "bootstrap"
+  runtime          = "provided.al2"
+
+  environment {
+    variables = {
+      DB_HOST         = module.db.db_instance_address
+      DB_PORT         = "5432"
+      DB_USER         = var.rds.db_user
+      DB_PASS         = var.rds.db_pass
+      DB_NAME         = var.rds.database_name
+      AWS_BUCKET_NAME = module.s3_bucket.s3_bucket_id
+    }
+  }
+
+  vpc_config {
+    subnet_ids         = module.vpc.database_subnets
+    security_group_ids = [module.lambda_sg.security_group_id]
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_cleanup.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = module.s3_bucket.s3_bucket_arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = module.s3_bucket.s3_bucket_id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s3_cleanup.arn
+    events              = ["s3:ObjectRemoved:*"]
+  }
+
+  depends_on = [
+    aws_lambda_permission.allow_bucket,
+    aws_iam_role_policy.lambda_s3_policy,
+    aws_lambda_function.s3_cleanup
+  ]
+}
--- role.tf
+++ role.tf
@@ -83,3 +83,63 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_task_role_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
   role       = aws_iam_role.ecs_execution_task_role.name
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name = "s3-cleanup-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_rds_access" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSDataFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_iam_role_policy" "lambda_s3_policy" {
+  name = "s3-cleanup-policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:*" # Berikan akses penuh untuk S3
+        ]
+        Resource = [
+          module.s3_bucket.s3_bucket_arn,
+          "${module.s3_bucket.s3_bucket_arn}/*"
+        ]
+      }
+    ]
+  })
+}
